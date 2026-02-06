6e39c804a23943e428ee514029e95377c1975bf6
gabriel <gabriel-baker@hotmail.com>
2026-02-06 14:05:10 +0100

    Move datashare.zip to data folder and update path

--- original.R
+++ original.R
@@ -33,3 +33,36 @@ characteristics <- characteristics |>
   rename_with(~ paste0("characteristic_", .), -c(permno, month, sic2)) |>
   drop_na(sic2) |>
   mutate(sic2 = as_factor(sic2))
+
+# Rank transform
+rank_transform <- function(x) {
+  rank_x <- rank(x)
+  rank_x[is.na(x)] <- NA
+  min_rank <- 1
+  max_rank <- length(na.omit(x))
+
+  if (max_rank == 0) {
+    # only NAs
+    return(rep(NA, length(x)))
+  } else {
+    return(2 * ((rank_x - min_rank) / (max_rank - min_rank) - 0.5))
+  }
+}
+
+characteristics <- characteristics |>
+  group_by(month) |>
+  mutate(across(contains("characteristic"), rank_transform)) |>
+  ungroup()
+
+# Replace NA with 0
+characteristics <- characteristics |>
+  group_by(month) |>
+  mutate(across(contains("characteristic"), \(x) {
+    replace_na(x, median(x, na.rm = TRUE))
+  })) |>
+  ungroup()
+
+characteristics <- characteristics |>
+  mutate(across(contains("characteristic"), \(x) replace_na(x, 0)))
+
+# CRSP
